fffd0bb9243c78afa1d18d73cfd0fd10b305c1df
Marcus Kanon <el_maco@hotmail.se>
2022-09-07 14:58:36 +0200

    Books service + models

---
+++ Bokulous-Back/Bokulous-Back/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Bokulous_Back.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string ISBN { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string[] Categories { get; set; }
+        public string Language { get; set; } = "";
+        public string[] Authors { get; set; }
+        public int Published { get; set; }
+        public int Weight { get; set; }
+        public bool IsUsed { get; set; }
+        public int InStorage { get; set; }
+        public double Price { get; set; }
+        public BookUser Seller { get; set; }
+    }
+}
