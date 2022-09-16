619ca14b6aa8df2366f5353933ee75b8ebc57aa8
Marcus Kanon <el_maco@hotmail.se>
2022-09-16 13:27:35 +0200

    Test class template

--- Bokulous-Back/Bokulous-BackTests/UsersTests.cs
+++ Bokulous-Back/Bokulous-BackTests/UsersTests.cs
@@ -5,11 +5,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Bokulous_Back.Services;
+using Bokulous_Back.Helpers;
+using Bokulous_Back.Controllers;
+using Bokulous_Back.Models;
 
 namespace Bokulous_Back.Tests
 {
     public class UsersTests
     {
+
+        BokulousDbService dbService = new("mongodb+srv://Bokulous:nwQjaj3eVzesn5P9@cluster0.vtut1fa.mongodb.net/test", "Bokulous");
+
+        private UserHelpers UserHelpers;
+        private UsersController UsersController;
+        public List<User?> TestUsers { get; set; }
+        public User? TestAdmin { get; set; }
+
+        public UsersTests()
+        {
+            UserHelpers = new(dbService);
+            UsersController = new(dbService);
+
+            TestUsers = new();
+
+            //Test admin
+            TestUsers.Add(new User()
+            {
+                IsActive = true,
+                IsAdmin = true,
+                IsBlocked = false,
+                IsSeller = false,
+                Mail = "bla1@bla.com",
+                Password = "hej123",
+                Previous_Orders = new UserBooks[0],
+                Username = "TEST_ADMIN"
+            });
+
+            //Test user
+            TestUsers.Add(new User()
+            {
+                IsActive = true,
+                IsAdmin = false,
+                IsBlocked = false,
+                IsSeller = false,
+                Mail = "bla2@bla.com",
+                Password = "hej123",
+                Previous_Orders = new UserBooks[0],
+                Username = "TEST_USER1"
+            });
+
+            //Test user
+            TestUsers.Add(new User()
+            {
+                IsActive = true,
+                IsAdmin = false,
+                IsBlocked = false,
+                IsSeller = false,
+                Mail = "bla3@bla.com",
+                Password = "hej123",
+                Previous_Orders = new UserBooks[0],
+                Username = "TEST_USER2"
+            });
+
+            //Adding test users to database
+            TestUsers.ForEach(async (user) => await dbService.CreateUserAsync(user));
+
+            TestUsers = dbService.GetUserAsync().Result;
+        }
+        public async Task ShowProfileTest()
+        {
+            var user = TestUsers.FirstOrDefault(x => x.Username == "TEST_USER1");
+
+            user.Password = null;
+
+            var expected = user;
+            var actual = UsersController.ShowProfile(user);
+
+            Assert.Equal(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        }
+
         [Fact()]
         public void TestMethodTest()
         {
